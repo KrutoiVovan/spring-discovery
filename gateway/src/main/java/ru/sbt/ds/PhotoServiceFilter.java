8d81c7b63e95a72592135c07e98284d9afb03c0a
Сафонов Владимир Владимирович <VVSafonov.SBT@sberbank.ru>
2017-05-17 13:54:27 +0300

    Доделано и с редиректом.

--- gateway/src/main/java/ru/sbt/ds/PhotoServiceFilter.java
+++ gateway/src/main/java/ru/sbt/ds/PhotoServiceFilter.java
@@ -1,15 +1,19 @@
 package ru.sbt.ds;
 
+import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import com.netflix.discovery.shared.Application;
 import com.netflix.discovery.shared.Applications;
+import com.netflix.loadbalancer.IPing;
+import com.netflix.loadbalancer.PingUrl;
+import com.netflix.loadbalancer.Server;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.netflix.zuul.filters.ProxyRequestHelper;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
+import java.util.Comparator;
 
 /**
  * Created by sbt-safonov-vv on 03.05.2017.
@@ -19,6 +23,8 @@ public class PhotoServiceFilter extends ZuulFilter {
 
     @Autowired
     private EurekaClient discoveryClient;
+
+    PingUrl pinger = new PingUrl();
 
     @Override
     public String filterType() {
@@ -35,35 +41,31 @@ public class PhotoServiceFilter extends ZuulFilter {
         return true;
     }
 
-    private String urlRedirectVersion1 = "localhost:45421/img?id=100";
 
-    private String urlRedirectVersion2 = "localhost:47000/img?id=100";
 
     @Override
     public Object run() {
         Applications apps = discoveryClient.getApplications();
         Application application = null;
-        Long curVersion = 0L;
-        Long version = 1L;
-        for (Application app : apps.getRegisteredApplications()){
-            if(app.getInstances().get(0) != null && app.getName().toLowerCase().equals("photo-service"))
-                try {
-                    version = Long.parseLong(app.getInstances().get(0).getMetadata().get("version"));
-                    if(version > curVersion){
-                        application = app;
-                        curVersion = version;
-                    }
-                } catch (NumberFormatException e){
-                    version = 1L;
-                }
-        }
+        application = apps.getRegisteredApplications()
+                .stream()
+                .filter(s -> s.getName().toLowerCase().equals("photo-service"))
+                .findFirst()
+                .get();
+
+        pinger.setSecure(false);
+        pinger.setPingAppendString("/img?id=100");
+
+        InstanceInfo target = application.getInstances()
+                .stream()
+                .sorted(Comparator.comparingLong(s -> Long.parseLong(s.getMetadata().get("version"))))
+                .filter(s -> pinger.isAlive(new Server(s.getHostName(), s.getPort())))
+                .findFirst()
+                .get();
 
         RequestContext ctx = RequestContext.getCurrentContext();
         try {
-            if(version == 1L)
-                ctx.setRouteHost(new URL(urlRedirectVersion1));
-            if(version == 2L)
-                ctx.setRouteHost(new URL(urlRedirectVersion2));
+            ctx.setRouteHost(new URL(target.getHomePageUrl()));
         } catch (Throwable e){
             System.out.println("went wrong!");
         }
--- photo-service-green/src/main/java/ru/sbt/ds/PhotoServiceApplication.java
+++ photo-service-green/src/main/java/ru/sbt/ds/PhotoServiceApplication.java
@@ -3,9 +3,11 @@ package ru.sbt.ds;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.PropertySource;
 
 @EnableEurekaClient
 @SpringBootApplication
+@PropertySource(value = "applicationGreen.properties")
 public class PhotoServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoServiceApplication.class, args);
